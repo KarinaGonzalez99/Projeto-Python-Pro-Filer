1eeeba0a3d33a5ce2476e283fae8f40ce1aa58bf
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-08-21 16:41:04 -0300

    Requisito 5

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -2,53 +2,57 @@ from unittest.mock import patch
 import pytest
 from pro_filer.actions.main_actions import show_disk_usage
 
+
 @pytest.fixture
-def context(tmp_path):
-    file1 = tmp_path / "file1.txt"
-    file1.write_text("example content" * 5)
-    file2 = tmp_path / "file2.txt"
-    file2.write_text("different content" * 3)
-    return {"all_files": [str(file1), str(file2)]}
+def base(tmp_path):
+    arquivoone = tmp_path / "arquivoone.txt"
+    arquivotwo = tmp_path / "arquivotwo.txt"
+    arquivoone.write_text("example content" * 5)
+    arquivotwo.write_text("different content" * 3)
+    return {"all_files": [str(arquivoone), str(arquivotwo)]}
 
-def test_show_disk_usage_all_in_one(context, capsys):
-    show_disk_usage(context)
-    captured = capsys.readouterr()
-    output_lines = captured.out.strip().split("\n")
 
-    assert len(output_lines) == 3
-    assert "file1.txt" in output_lines[0]
-    assert "file2.txt" in output_lines[1]
-    assert "Total size:" in output_lines[2]
+def teste1 (base, capsys):
+    show_disk_usage(base)
+    save = capsys.readouterr()
+    ordem = save.out.strip().split("\n")
+    assert len(ordem) == 3
+    assert "arquivoone.txt" in ordem[0]
+    assert "arquivotwo.txt" in ordem[1]
+    assert "Total size:" in ordem[2]
 
-    context["all_files"] = []
-    show_disk_usage(context)
-    captured = capsys.readouterr()
-    output_lines = captured.out.strip().split("\n")
 
-    assert len(output_lines) == 1
-    assert "Total size:" in output_lines[0]
-    assert "0" in output_lines[0]
+def test_show_disk_usage_empty_base(capsys):
+    base = {"all_files": []}
+    show_disk_usage(base)
+    save = capsys.readouterr()
+    ordem = save.out.strip().split("\n")
+    assert len(ordem) == 1
+    assert "Total size:" in ordem[0]
+    assert "0" in ordem[0]
 
-    show_disk_usage(context)
-    captured = capsys.readouterr()
-    output_lines = captured.out.strip().split("\n")
 
+def teste2 (base, capsys):
+    show_disk_usage(base)
+    save = capsys.readouterr()
+    ordem = save.out.strip().split("\n")
     sizes = [
-        int(line.split("(")[1].split("%")[0]) for line in output_lines[:-1]
+        int(line.split("(")[1].split("%")[0]) for line in ordem[:-1]
     ]
-
     assert sizes == sorted(sizes, reverse=True)
 
-    context["all_files"].append("non_existent_file.txt")
 
+def teste3 (base):
+    base["all_files"].append("non_existent_file.txt")
     with pytest.raises(FileNotFoundError):
-        show_disk_usage(context)
+        show_disk_usage(base)
 
+
+def teste4 (base, capsys):
     with patch(
         "pro_filer.actions.main_actions.os.path.getsize", return_value=150
     ):
-        show_disk_usage(context)
-        captured = capsys.readouterr()
-        output_lines = captured.out.strip().split("\n")
-
-        assert "Total size: 300" in output_lines[-1]
+        show_disk_usage(base)
+        save = capsys.readouterr()
+        ordem = save.out.strip().split("\n")
+        assert "Total size: 300" in ordem[-1]
