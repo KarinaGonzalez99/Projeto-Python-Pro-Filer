bfe9b3c3cf5e605a16d7ebd539777660aa939b55
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-08-21 13:27:48 -0300

    Requisito 2

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -12,6 +12,16 @@ def show_deepest_file(context):
         print(f"Deepest file: {deepest_file}")
 
 
+def normalize_string(s, case_sensitive):
+    return s if case_sensitive else s.lower()
+
+
+def file_contains_search_term(file_name, search_term, case_sensitive):
+    normalized_file_name = normalize_string(file_name, case_sensitive)
+    normalized_search_term = normalize_string(search_term, case_sensitive)
+    return normalized_search_term in normalized_file_name
+
+
 def find_file_by_name(context, search_term, case_sensitive=True):
     if not search_term:
         return []
@@ -21,11 +31,7 @@ def find_file_by_name(context, search_term, case_sensitive=True):
     for path in context["all_files"]:
         file_name = path.split("/")[-1]
 
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
-
-        if search_term in file_name:
+        if file_contains_search_term(file_name, search_term, case_sensitive):
             found_files.append(path)
 
     return found_files
